5fd39f409064f52cb79216db719aa057c72a9db4
Kamui <arnabmandal952@gmail.com>
2026-01-01 23:25:22 +0530

    Add files via upload

---
+++ web/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        discord: {
+          blurple: '#5865F2',
+          green: '#57F287',
+          yellow: '#FEE75C',
+          fuchsia: '#EB459E',
+          red: '#ED4245',
+          dark: '#23272A',
+          gray: '#2C2F33',
+          lightgray: '#99AAB5',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
